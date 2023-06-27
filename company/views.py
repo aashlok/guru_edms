99207c1fc61f6cb896e6d37edb2df6de3b795867
Aashish Lokhande <ferrous.aashish@gmail.com>
2023-06-27 23:11:12 +0530

    Add Configuration view and template

--- company/views.py
+++ company/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
 
 from .models import Company, Designation, Employee
 from .forms import CompanySetupForm
+from project.models import Project
+from document.models import Document, DocumentFile, DocumentType
 
 # Create your views here.
 
@@ -21,14 +24,18 @@ Username is auto generated so no UserCreationForm.
 Only set password from input.
 Creates specified folder as central file server.
 """
-    form = CompanySetupForm()
+    count = Company.objects.all().count()
+    if count == 1:
+        messages.info(request, 'Company is already set up, please login')
+        return redirect('login')
 
     if request.method == 'POST':
         form = CompanySetupForm(request.POST)
         newcompany = form.save(commit=False)
         short_code = request.POST.get('short_code')
         fileserver_folder = (request.POST.get('fileserver_folder')
-                             + '.' + short_code)
+                             + '/' + short_code)
+        newcompany.fileserver_folder = fileserver_folder
         password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
 
@@ -38,31 +45,50 @@ Creates specified folder as central file server.
             return HttpResponse('Passwords do not match')
 
         newuser = User.objects.create(
+            first_name='admin',
+            last_name=short_code,
             username='admin' + '.' + short_code,
-            password=password,
             is_staff=True,
         )
 
         os.makedirs(fileserver_folder, exist_ok=True)  # TO-DO: try
+        newuser.set_password(password)
         newuser.save()
         newcompany.save()
-        return redirect('configuration')
+        return redirect('login')
 
     else:
         form = CompanySetupForm()
-        # return HttpResponse('Error occured in initial setup')
 
     context = {'form': form}
     return render(request, 'company/initial-setup.html', context=context)
 
 
+@login_required
+@staff_member_required
 def configuration(request):
-    context = {}
+    """
+View and add basic configuration before commencing usage.
+From this page only employees, designations and document types 
+can be added or edited.
+Shows summary of configuration.
+"""
+    num_employees = Employee.objects.all().count()
+    num_designations = Designation.objects.all().count()
+    num_doc_types = DocumentType.objects.all().count()
+
+    context = {
+        'num_employees': num_employees,
+        'num_designations': num_designations,
+        'num_doc_types': num_doc_types
+    }
     return render(request, 'company/configuration.html', context=context)
 
 
 @login_required
 def index(request):
+    if request.user.is_staff:
+        return redirect('company:configuration')
 
     context = {}
     return render(request, 'index.html', context=context)
