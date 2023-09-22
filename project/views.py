810c87280520ee7acc07329f5943c3383fcf12e2
Aashish Lokhande <ferrous.aashish@gmail.com>
2023-09-22 22:56:00 +0530

    Modify document list showing last file

--- project/views.py
+++ project/views.py
@@ -72,11 +72,28 @@ class ProjectDetailView(LoginRequiredMixin, generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        doc_list = []
+        user_doc_list = []  # document only list
+        doc_file_list = []  # document with last file activity composite list
+
         if self.request.user.employee.designation.project_crud_permission:
-            doc_list = self.object.document_set.all
+            user_doc_list = self.object.document_set.all()
         else:
-            doc_list = self.object.document_set.filter(
+            user_doc_list = self.object.document_set.filter(
                 document_user=self.request.user.employee.id)
-        context['doc_list'] = doc_list
+
+        for doc in user_doc_list:
+            last_file = doc.documentfile_set.last()
+            doc_last_file = {}  # dictionary for key value access in template
+            doc_last_file['id'] = doc.id
+            doc_last_file['name'] = doc.label
+            doc_last_file['user'] = doc.document_user
+            doc_last_file['save'] = doc.save_count
+            doc_last_file['file_id'] = last_file.id
+            doc_last_file['last_file_name'] = last_file.filename
+            doc_last_file['last_file_created'] = last_file.created_date.date()
+            doc_last_file['last_file_due'] = last_file.due_date.date()
+            doc_last_file['last_file_status'] = last_file.get_status_display
+            doc_file_list.append(doc_last_file)
+
+        context['doc_file_list'] = doc_file_list
         return context
